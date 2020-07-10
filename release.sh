ebca07dbe6139ae8f565d5d84f55be4f123335d7
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2020-07-11 00:33:24 +0900

    release.sh: Remove remote gh-pages branch before pushing
    This is to mimic force push.

--- release.sh
+++ release.sh
@@ -9,6 +9,7 @@ PUBLIC_URL="https://genki-s.github.io/toruneko-3-tools/" elm-app build
 
 git add -f build
 git commit -m "Release"
+git push origin --delete gh-pages
 git subtree push --prefix build origin gh-pages
 
 git checkout -
